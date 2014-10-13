36a70c6ac9e89e5ccc409446291090035cbdd1e5
Nathan Smith <tornatorkr@fullsail.edu>
2014-10-13 00:24:24 -0400

    variables declared with prompts

--- WPF_master/Assignment_Expressions/Expressions_Industry/js/script.js
+++ WPF_master/Assignment_Expressions/Expressions_Industry/js/script.js
@@ -2,3 +2,17 @@
 //This calculator will determine a call center representative's projected call volume for the month
 //input required will be: call center agent's first and last name; average calls per day; number of dropped or unresolved calls, days worked per month
 
+var firstName; 
+var lastName;
+var ein;//Employee Identification Number
+var fullName = [firstName, lastName, ein];//array 
+var averageCallsDaily;
+var droppedCalls; 
+var daysWorked; 
+
+firstName = prompt("I will calculate your projected number of completed calls for the month.\nPlease enter your first name.");
+lastName = prompt("Please enter your last name.");
+ein = prompt("Please enter your 5-digit Employee Identification Number.");
+averageCallsDaily = prompt("Please enter the average number of received calls per day.");
+droppedCalls = prompt("Please enter the average number of dropped or unresolved calls per day.");
+daysWorked = prompt("Please enter the number of working days for this month.");
