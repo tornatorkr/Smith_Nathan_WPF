b2ff7a37181e59ed5538ed445e5b89692cd6d9b0
Nathan Smith <tornatorkr@fullsail.edu>
2014-10-12 23:46:10 -0400

    Setup part 2
    initial remarks

--- WPF_master/Assignment_Expressions/Expressions_Industry/js/script.js
+++ WPF_master/Assignment_Expressions/Expressions_Industry/js/script.js
@@ -1,1 +1,4 @@
-alert("This is in the js file");
+// Nathan Smith - Oct. 12, 2014 - Assignment: Expressions - Industry Calulator
+//This calculator will determine a call center representative's projected call volume for the month
+//input required will be: call center agent's first and last name; average calls per day; number of dropped or unresolved calls, days worked per month
+
