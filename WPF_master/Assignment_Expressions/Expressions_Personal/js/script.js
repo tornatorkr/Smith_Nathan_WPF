b6a530dc9992355b83261c7fbbdcc5456b2ea918
Nathan Smith <tornatorkr@fullsail.edu>
2014-10-12 23:06:28 -0400

    first prompt
    corrected title page and added first prompt

--- WPF_master/Assignment_Expressions/Expressions_Personal/js/script.js
+++ WPF_master/Assignment_Expressions/Expressions_Personal/js/script.js
@@ -1,1 +1,10 @@
-alert("This is in the js file");
+// Nathan Smith - Oct. 12, 2014 - Assignment: Expressions - Personal Calulator
+//This calculator will determine the expense for gasoline per week
+//input required will be: current cost per gallon; averge miles per gallon for the vehicle; miles traveled per week
+
+//Declaration and assignment
+
+var price; 
+var mpg;
+var distance; 
+price = prompt("I will calculate the amount that you spend on gasoline in a week's time.\nPlease enter the current price of gas per gallon.");
