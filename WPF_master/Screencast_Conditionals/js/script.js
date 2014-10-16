38c12d4e99a711a487ef3c91fd377010e38c2b0f
tornatorkr <tornatorkr@fullsail.edu>
2014-10-16 10:40:30 -0400

    Screencast_Conditional_Logic
    if

---
+++ WPF_master/Screencast_Conditionals/js/script.js
@@ -0,0 +1,9 @@
+// Conditional Loggic
+
+var oldEnough = true;//Boolean
+
+//If child is old enough, print to console, "you can ride!"
+if(oldEnough){
+	//code performed if condition is met or true
+	console.log("You can ride the coaster!");
+}
