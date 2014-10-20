dc617f05a9d94b52afaed0c3592bd27c3dc0f672
tornatorkr <tornatorkr@fullsail.edu>
2014-10-20 10:46:24 -0400

    Screencast_Functions
    basic structure

---
+++ WPF_master/Screencast_Functions/js/script.js
@@ -0,0 +1,12 @@
+// Conditional Logic
+
+var kidheight = prompt("Plaese enter your height in inches.");//Relational Expressions
+
+//If child is old enough, print to console, "you can ride!"
+if(kidheight > 48){
+	//code performed if condition is met or true
+	console.log("You can ride the coaster!");
+}else{
+	console.log("No soup for you!");
+	
+}
