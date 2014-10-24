28a2023d0695835eb462bcf8ddf9038b92a87147
tornatorkr <tornatorkr@fullsail.edu>
2014-10-24 11:15:35 -0400

    For Loops

--- WPF_master/Screencast_Loops/js/script.js
+++ WPF_master/Screencast_Loops/js/script.js
@@ -2,6 +2,8 @@
 
 //While Loop
 
+console.log("________WHILE LOOPS________");
+
 var b = 25; //value of "b" sets the number of times of repetition
 while(b > 0){
 	
@@ -11,6 +13,7 @@ while(b > 0){
 }
 
 //Do While Loops
+console.log("________DO WHILE LOOPS________");
 
 var c = 10;
 do{
@@ -19,3 +22,13 @@ do{
 	c--;
 }
 while(c > 0);//with do while loop the code within the loopis performed at least once even if the condition is false
+
+//For Loops
+console.log("________FOR LOOPS________");
+
+for(var i = 10; i > 0; i--){//sets index, condition and increment or decrement all in the same statement
+	
+	console.log(i + " ducks in a row.");
+	
+	
+}
