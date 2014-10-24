2441aa3f5ef5bdc42ee6b52d020f123d179a3647
tornatorkr <tornatorkr@fullsail.edu>
2014-10-24 10:43:13 -0400

    While Loop

---
+++ WPF_master/Screencast_Loops/js/script.js
@@ -0,0 +1,11 @@
+// Loops
+
+//While Loop
+
+var b = 25; //value of "b" sets the number of times of repetition
+while(b > 0){
+	
+	console.log(b + " ducks in a row.");
+	b--; //subtracts 1 from b each time through the loop
+	
+}
