ab6f102d14ca6e47a4edbfe3a8a6f827cde90534
tornatorkr <tornatorkr@fullsail.edu>
2014-10-24 11:05:58 -0400

    Do While Loop

--- WPF_master/Screencast_Loops/js/script.js
+++ WPF_master/Screencast_Loops/js/script.js
@@ -9,3 +9,13 @@ while(b > 0){
 	b--; //subtracts 1 from b each time through the loop
 	
 }
+
+//Do While Loops
+
+var c = 10;
+do{
+	
+	console.log(c + " ducks in a row.");
+	c--;
+}
+while(c > 0);//with do while loop the code within the loopis performed at least once even if the condition is false
